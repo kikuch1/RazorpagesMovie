d56ed0b38b74240f965020b204fbd97cef067315
kikuch1 <atera376zaw@gmail.com>
2020-08-01 16:22:38 +0900

    チュートリアル７・検証の追加を完了しました。

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -11,20 +12,31 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; }
 
         [Display(Name = "タイトル")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+
 
         [Display(Name = "発売日")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
         [Display(Name = "ジャンル")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
 
         [Display(Name = "価格（税込）")]
         [Column(TypeName = "decimal(18, 2)")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
         [Display(Name = "レート")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
