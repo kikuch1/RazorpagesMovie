e1f92ef90526c79a1b6db4b04a50d44aef05cc94
kikuch1 <atera376zaw@gmail.com>
2020-08-01 16:01:37 +0900

    チュートリアル６・新しいフィールドの追加を完了しました。

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -23,5 +23,8 @@ namespace RazorPagesMovie.Models
         [Display(Name = "価格（税込）")]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [Display(Name = "レート")]
+        public string Rating { get; set; }
     }
 }
